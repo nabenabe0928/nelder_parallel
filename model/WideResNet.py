15b41b4601bac0064e23226385df139f9cd2220b
nabenabe0928 <47781922+nabenabe0928@users.noreply.github.com>
2019-05-09 14:12:34 +0900

    changed the way to dropout
    dropout2d to dropout
    
    I referred here (https://pytorch.org/docs/stable/nn.html)
    
    the difference
    In dropout, only some parts of tensor (say 20% of elements in a tensor) would be zeroed out. On the other hands, in dropout2d, all of elements would be zeroed out by the probability of 20%.
    
    dropout
    During training, randomly zeroes some of the elements of the input tensor with probability p using samples from a Bernoulli distribution.
    
    dropout2d
    Randomly zero out entire channels (a channel is a 2D feature map, e.g., the jj-th channel of the ii-th sample in the batched input is a 2D tensor \text{input}[i, j]input[i,j]) of the input tensor). Each channel will be zeroed out independently on every forward call with probability p using samples from a Bernoulli distribution.

--- model/WideResNet.py
+++ model/WideResNet.py
@@ -48,7 +48,8 @@ class BasicBlock(nn.Module):
     def forward(self, x):
         h = F.relu(self.bn1(x))
         h = self.c1(h)
-        h = F.dropout2d(h, p = self.drop_rate, training = self.training)
+        #h = F.dropout2d(h, p = self.drop_rate, training = self.training)
+        h = F.dropout(h, p = self.drop_rate, training = self.training)
         h = F.relu(self.bn2(h))
         h = self.c2(h)
 
